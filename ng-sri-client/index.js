91f08a9f2e683a7df96861121ebf34666aeefa55
matthiassnellings <matthias.snellings@katholiekonderwijs.vlaanderen>
2018-11-28 12:07:46 +0000

    fix angular client

--- ng-sri-client/index.js
+++ ng-sri-client/index.js
@@ -1,9 +1,7 @@
 /* global angular*/
-var module = angular.module('ng-sri-client', ['ngSanitize']);
-
-module.provider('sriClientConfiguration', require('./sri-client-configuration.js'));
-
 module.exports = function(configurations) {
+  var module = angular.module('ng-sri-client', ['ngSanitize']);
+
   if(!Array.isArray(configurations)) {
     configurations = [configurations];
   }
