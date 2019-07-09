4efed703339d2a696030a8e2b1a51aef6e0f5574
Snellings Matthias <matthias.snellings@vsko.be>
2019-07-09 14:17:35 +0200

    fix to formatPhoneNumber for Gent en Luik

--- contactdetail-utils/contactdetail-utils.js
+++ contactdetail-utils/contactdetail-utils.js
@@ -36,7 +36,7 @@ const formatPhoneNumber = function(phone, formatBelgianInternationalNumbers) {
     } else if(newValue.length === 9) {
       if(newValue.substring(0,2) === '02' || newValue.substring(0,2) === '03') {
         newValue = newValue.substring(0,2) + ' ' + newValue.substring(2,5) + ' ' + newValue.substring(5,7) + ' ' + newValue.substring(7);
-      } else if( (newValue.substring(0,2) === '04' || newValue.substring(0,2) === '09') && (newValue.substring(2,3) === 2 || newValue.substring(2,3) === 3) ) {
+      } else if( (newValue.substring(0,2) === '04' || newValue.substring(0,2) === '09') && (newValue.substring(2,3) === '2' || newValue.substring(2,3) === '3') ) {
         newValue = newValue.substring(0,2) + ' ' + newValue.substring(2,5) + ' ' + newValue.substring(5,7) + ' ' + newValue.substring(7);
       } else if(newValue.substring(0,1) === '0') {
         newValue = newValue.substring(0,3) + ' ' + newValue.substring(3,5) + ' ' + newValue.substring(5,7) + ' ' + newValue.substring(7);
